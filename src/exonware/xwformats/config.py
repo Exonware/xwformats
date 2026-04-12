c4003a250be43917778f72f05fd1b24a0fc281cc
Muhammad AlShehri <malshehri@exonware.com>
2026-04-13 00:09:48 +0300

    Update version to 0.9.0.33 - Final commit

--- src/exonware/xwformats/config.py
+++ src/exonware/xwformats/config.py
@@ -5,7 +5,7 @@ Configuration classes for xwformats.
 Company: eXonware.com
 Author: eXonware Backend Team
 Email: connect@exonware.com
-Version: 0.9.0.32
+Version: 0.9.0.33
 Generation Date: 07-Jan-2025
 """
 
